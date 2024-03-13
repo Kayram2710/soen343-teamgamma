1022e678c7aecdc3b9cfa73aa3ab76ecfcacabbb
Karim <kimomomo@hotmail.com>
2024-03-13 15:50:10 -0400

    Create Sim.java

---
+++ smart-home/src/main/java/simulation/Sim.java
@@ -0,0 +1,25 @@
+package simulation;
+
+public class Sim {
+
+
+
+    /*
+     * Need to Implement back-end functionalities for:
+     * Set-Date and time
+     * 
+     * 
+     */
+
+
+    //call this command to start simulation
+    public void run(){
+
+    }
+
+    //call this command to stop simulation
+    public void stop(){
+
+    }
+
+}
