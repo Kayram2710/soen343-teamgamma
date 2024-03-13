aee11e9a6a826a6e13c0adb99a03e1f4f6b3db01
Karim <kimomomo@hotmail.com>
2024-03-13 16:13:16 -0400

    Update Sim.java

--- smart-home/src/main/java/simulation/Sim.java
+++ smart-home/src/main/java/simulation/Sim.java
@@ -2,15 +2,28 @@ package simulation;
 
 public class Sim {
 
-
-
     /*
-     * Need to Implement back-end functionalities for:
-     * Set-Date and time
+     * The Simulation will run once the house layout is displayed.
+     * It is a toggle that will simulate the house environment once certain parameters are passed:
      * 
+     * Pre-Start of Simulation:
+     * Profile Selection (from existing profile)
+     * Set-Date and time
+     * Set House location
+     * Select profile permissions
+     * 
+     * 
+     * Post-preparations:
+     * Move logged-in user
+     * Change simulation run time
+     * Start/Stop Sim
+     * Modify Date and Time
+     * Modify Temp Outside Home
+     * Block Windows from closing
      * 
      */
 
+    
 
     //call this command to start simulation
     public void run(){
