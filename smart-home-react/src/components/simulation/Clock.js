7c10772baef5bd3874074e81a9b23ef7ff7ee897
Sisahga Phimmasone <80082494+Sisahga@users.noreply.github.com>
2024-03-25 12:48:52 -0400

    implemented clock and speed #18

--- smart-home-react/src/components/simulation/Clock.js
+++ smart-home-react/src/components/simulation/Clock.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import './sim.css';
+import React, { useState, useEffect } from "react";
+import "./sim.css";
 
-const Clock = ({isActive, speed, date}) => {
-
+const Clock = ({ isActive, speed, date }) => {
   const [seconds, setSeconds] = useState(0);
 
   const datetime = new Date(date);
 
   useEffect(() => {
+    console.log("Clock.js: speed: ", speed);
+
     let interval;
 
     if (isActive) {
       interval = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + speed);
-      }, 1);
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000 / speed);
     } else {
       clearInterval(interval);
       setSeconds(0);
     }
 
     return () => clearInterval(interval);
-  }, [isActive]);
+  }, [isActive, speed]);
 
   const formatTime = (totalSeconds) => {
-
-    datetime.setTime(datetime.getTime() + (totalSeconds * 1000));
+    datetime.setTime(datetime.getTime() + totalSeconds * 1000);
 
     const year = datetime.getFullYear();
-    const month = ('0' + (datetime.getMonth() + 1)).slice(-2); // Month is zero-indexed, so we add 1 and pad with '0'
-    const day = ('0' + datetime.getDate()).slice(-2);
-    const hours = ('0' + datetime.getHours()).slice(-2);
-    const minutes = ('0' + datetime.getMinutes()).slice(-2);
-    const seconds = ('0' + datetime.getSeconds()).slice(-2);
+    const month = ("0" + (datetime.getMonth() + 1)).slice(-2); // Month is zero-indexed, so we add 1 and pad with '0'
+    const day = ("0" + datetime.getDate()).slice(-2);
+    const hours = ("0" + datetime.getHours()).slice(-2);
+    const minutes = ("0" + datetime.getMinutes()).slice(-2);
+    const seconds = ("0" + datetime.getSeconds()).slice(-2);
 
     const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
@@ -41,10 +41,12 @@ const Clock = ({isActive, speed, date}) => {
   return (
     <div>
       <div id="timer-display" className="flex align-center topCtnPadding">
-        Simulation Time:<br/>
-        {formatTime(seconds)}</div>
+        Simulation Time:
+        <br />
+        {formatTime(seconds)}
+      </div>
     </div>
   );
-}
+};
 
 export default Clock;
